16dc131c6f7212cf8f4aab7c9e31fa84e6332d45
Ryanliszewski <ryanliszewski@gmail.com>
2018-02-20 16:38:28 -0800

    List view displays data correctly. Needs more styling

--- CRNA/App.js
+++ CRNA/App.js
@@ -1,85 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, ScrollView, Image, Button, FlatList } from 'react-native';
 
-// import COMMUNITY_MEMBERS from './assets/constants';
+import { COMMUNITY_MEMBERS } from './assets/constants'
 
-const COMMUNITY_MEMBERS = [
-  {
-    name: 'Monte Thakkar',
-    image: 'https://avatars2.githubusercontent.com/u/7840686?s=400&v=4',
-    github_username: 'monte9'
-  },
-  {
-    name: 'Ryan Liszewski',
-    image: 'https://avatars3.githubusercontent.com/u/5275250?s=460&v=4',
-    github_username: 'ryanliszewski'
-  },
-  {
-    name: 'Thomas Zhu',
-    image: 'http://thomaszhu.com/resources/thomas_avatar.jpeg',
-    github_username: 'thomashzhu'
-  },
-  {
-    name: 'Bhavesh Chowdhury',
-    image: 'https://avatars3.githubusercontent.com/u/13439148?s=460&v=4',
-    github_username: 'bhaveshc789'
-  },
-  {
-    name: 'Sukhjit Singh',
-    image: 'https://avatars1.githubusercontent.com/u/18251293?s=400&u=1ee2922f2dd90d94bb4efbec7cc815ef510a0ad7&v=4',
-    github_username: 'sukhjitsingh'
-  },
-  {
-    name: 'Prakash Gurung',
-    image: 'https://avatars.githubusercontent.com/makkhay',
-    github_username: 'makkhay'
-  },
-  {
-    name: 'Nicholas Szeto',
-    image: 'https://avatars1.githubusercontent.com/u/23561635?s=460&v=4',
-    github_username: 'niszeto'
-  },
-  {
-    name: 'Emanuel Saunders',
-    image: 'https://scontent-lax3-2.cdninstagram.com/vp/1ee634d2bdf29d5a327858a15ccfdf42/5B26F2E8/t51.2885-19/s150x150/18160691_1198139890329419_5383374010954285056_a.jpg',
-    github_username: 'saundemanu'
-  },
-  {
-    name: 'William Hua',
-    image: 'https://avatars3.githubusercontent.com/u/36139326?s=460&v=4',
-    github_username: 'william-hua'
-  },
-  {
-    name: 'Mitul Savani',
-    image: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/18556546_1553987881278489_849408995015005895_o.jpg?oh=bb65219475a9cbac85ca7090e7ee877b&oe=5ADE398B',
-    github_username: 'mitulsavani'
-  },
-  {
-    name: 'Jakhongir Khusanov',
-    image: 'https://avatars0.githubusercontent.com/u/25942541?s=460&v=4',
-    github_username: 'jkhusanov'
-  },
-  {
-    name: 'Teodora Caneva',
-    image: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-1/c44.0.320.320/p320x320/20914645_1579156895438534_1027285013181250707_n.jpg?oh=0ff1b3c4c8bc8e00edb84cecd660d0fe&oe=5B1C897A',
-    github_username: 'kodekat'
-  },
-  {
-    name: 'Affaan Ghazzali',
-    image: 'https://avatars0.githubusercontent.com/u/32111151?s=460&v=4',
-    github_username: 'affaanghazzali'
-  },
-  {
-    name: 'Girish Rawat',
-    image: 'https://avatars2.githubusercontent.com/u/6697235?s=460&v=4',
-    github_username: 'GirishRawat'
-  },
-  {
-    name: 'Karan Gupta',
-    image: 'https://avatars1.githubusercontent.com/u/19352928?s=460&v=4',
-    github_username: 'karanguptak9'
-  }
-]
 
 export default class App extends React.Component {
   
@@ -104,8 +27,14 @@ export default class App extends React.Component {
         
           <Image source={require('./assets/Prince.png')} style={styles.largeImage}/>
           <View style={styles.imageContainer}> 
-            <Image source={require('./assets/Prince.png')} style={styles.smallImage}/>
-            <Image source={require('./assets/Prince.png')} style={styles.smallImage}/>
+            <Image 
+              source={require('./assets/Prince.png')} 
+              style={styles.smallImage}
+            />
+            <Image 
+              source={require('./assets/Prince.png')} 
+              style={styles.smallImage}
+            />
           </View>
           <View 
             style={styles.toggleContainer}
@@ -121,7 +50,22 @@ export default class App extends React.Component {
           <FlatList
             style={styles.list}
             data={COMMUNITY_MEMBERS}
-            renderItem={({item}) => <Text style={styles.item}> {item.name} </Text>}
+            renderItem={ ({item, separators}) => (
+              <View style={styles.itemContainer}>
+                <Image 
+                  source={{uri: item.image}} 
+                  style={
+                    { width: 60, 
+                      height: 60,
+                      borderRadius: 30
+                    }}
+                />
+                
+                  <Text style={styles.itemName}> {item.name} </Text>
+                  <Text style={styles.itemGitName}> {item.github_username} </Text>
+                
+              </View>
+            )}
           />
       </ScrollView>
     );
@@ -130,9 +74,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
     list: {
-    paddingTop: 50,
     flex:1,
-    height: 1000,
     backgroundColor: '#fff',
   },
 
@@ -145,12 +87,29 @@ const styles = StyleSheet.create({
   toggleButton: {
   },
 
-  item: {
-    width: 200,
-    height: 200,
-    fontSize: 15,
+  itemContainer: {
     padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flex: 1,
+  },
+
+  itemTextContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+
+  itemName: {
+    justifyContent: 'space-between',
+    flexDirection: 'column',
+    flex: 1,
+    fontSize: 15,
     backgroundColor:'#fff',
+    padding: 0,
+  },
+
+  itemGitName:{
+    fontSize: 10,
   },
 
   imageContainer: {
